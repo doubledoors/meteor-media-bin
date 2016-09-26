6120fa88adc9378828406a8b6b3ba411a83cf4cf
Ben Sharp <bfwsharp@gmail.com>
2016-09-26 17:37:39 +0100

    add Header component

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
+import Header from './Header.jsx';
+
 export default class App extends Component {
 
   render() {
     return (
       <div className="container">
-        <header>
-          <h1>Header</h1>
-        </header>
+        <Header />
       </div>
     );
   }
